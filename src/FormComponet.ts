e25163a846e5aad88b8c89f0db87271b6090f2ff
winpzs <winpzs@gmail.com>
2017-06-14 21:38:40 +0800

    form componet

--- src/FormComponet.ts
+++ src/FormComponet.ts
@@ -6,13 +6,13 @@ import { Componet, VM } from "cmpx";
     <div class="row">
         <span class="text">input test</span>
         <span class="input">
-            <input type="text" value="{{#this.model.input}}" />
+            <input type="text" model="{{#this.model.input}}" />
         </span>
     </div>
     <div class="row">
         <span class="text">select test</span>
         <span class="input">
-            <select value="{{#this.model.select}}">
+            <select model="{{#this.model.select}}">
                 <option value="">请选择</option>
                 <option value="1">option1</option>
                 <option value="2">option2</option>
@@ -22,20 +22,20 @@ import { Componet, VM } from "cmpx";
     <div class="row">
         <span class="text">checkbox value</span>
         <span class="input">
-            <input type="checkbox" checked="{{#this.model.checked}}" />
+            <input type="checkbox" value="1" checked="{{#this.model.checked}}" />
         </span>
     </div>
     <div class="row">
         <span class="text">textarea test</span>
         <span class="input">
-            <textarea value="{{#this.model.textarea}}"></textarea>
+            <textarea model="{{#this.model.textarea}}"></textarea>
         </span>
     </div>
     <div>
         <span class="text">结果</span>
         <span class="input">
             <textarea style="width:500px;height:100px;"
-                value="{{JSON.stringify(this.model)}}"></textarea>
+                model="{{JSON.stringify(this.model)}}"></textarea>
         </span>
     </div>
 </div>`,

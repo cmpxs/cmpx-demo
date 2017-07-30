92458dcedca88c6d56056030b442e8fa0e4f58bd
winpzs <winpzs@gmail.com>
2017-07-30 17:58:07 +0800

    Updates

--- expression/BindDemoComponet.d.ts
+++ expression/BindDemoComponet.d.ts
@@ -1,6 +1,7 @@
 import { Componet } from 'cmpx';
 export default class BindDemoComponet extends Componet {
     constructor();
+    tick(): number;
     text: string;
     changeText(): void;
     onUpdate(cb: any): void;
